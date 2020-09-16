9bbb9cc84c9e0b27d4fca575a7cb33077848ced0
ckopec <ckopec@princeton.edu>
2020-09-16 09:38:02 -0400

    Change virmen_specific comm names

--- test_send_VirmenVars.m
+++ test_send_VirmenVars.m
@@ -9,7 +9,7 @@ trainee = virmen_utils.get_test_trainee_Virmen();
 virmen_structure.trainee_file = virmen_utils.struct2binary(trainee);
 
 code_files = ...
-    comm.comm_virmen_specific.generate_send_codes_struct( virmen_structure );
+    comm.virmen_specific.generate_send_codes_struct( virmen_structure );
 
 
   vr.tcp_client = comm.tcp.initialize_tcp( ...
@@ -18,5 +18,5 @@ code_files = ...
       'server', ...
       0);
   
-send_all_virmen_vars(tcp_client, codes_files, virmen_structure);
+comm.virmen_specific.send_all_virmen_vars(tcp_client, codes_files, virmen_structure);
 
